c07434f74eef83d00c66cb1f89bf859ac227bef3
Eldhose John <dits59@yahoo.com>
2014-01-10 17:56:49 +0530

    Added project skeleton.

---
+++ hangman/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import debug_toolbar
+
+urlpatterns = patterns('',
+    url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    # Examples:
+    # url(r'^$', 'hangman.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
